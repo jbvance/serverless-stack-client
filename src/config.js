77d7609acf9e9830df6859c4944b220abd0d75f5
Jason Vance <jbvan@LAPTOP-JME4BMN6.NSLLP.local>
2020-03-28 08:23:39 -0500

    add Amplify configuration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-uploads-jv"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://piimwcxts9.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_ZgMa3S9W9",
+    APP_CLIENT_ID: "64crjthh83kk94bisv5g5l8h0k",
+    IDENTITY_POOL_ID: "us-east-2_ZgMa3S9W9"
+  }
+};
