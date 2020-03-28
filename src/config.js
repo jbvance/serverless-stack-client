e6974bb88895665d7005f38d52de9a1ecb34ac55
Jason Vance <jbvan@LAPTOP-JME4BMN6.NSLLP.local>
2020-03-28 11:44:00 -0500

    Add login, logout, and create note

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-2",
     BUCKET: "notes-app-uploads-jv"
@@ -11,6 +12,6 @@ export default {
     REGION: "us-east-2",
     USER_POOL_ID: "us-east-2_ZgMa3S9W9",
     APP_CLIENT_ID: "64crjthh83kk94bisv5g5l8h0k",
-    IDENTITY_POOL_ID: "us-east-2_ZgMa3S9W9"
+    IDENTITY_POOL_ID: "us-east-2:a926c721-ef6f-4816-b62f-04d01c216499"
   }
 };
